0fb38e05c4bee4973dc22910af6c5797205d77c2
polar <antronset@gmail.com>
2023-02-13 16:38:45 +0100

    Binary add recursive.

--- src/main/java/by/trayan/algorithms/leetcode/chapter1/BinarySearch.java
+++ src/main/java/by/trayan/algorithms/leetcode/chapter1/BinarySearch.java
@@ -3,7 +3,8 @@ package by.trayan.algorithms.leetcode.chapter1;
 public class BinarySearch {
 
     public static int search(int[] nums, int target) {
-        if (nums == null || nums.length == 0) return -1;
+        if (nums == null || nums.length == 0)
+            return -1;
         int left = 0;
         int right = nums.length - 1;
         while (left <= right) {
@@ -11,6 +12,19 @@ public class BinarySearch {
             if (nums[mid] == target) return mid;
             if (nums[mid] > target) right = mid - 1;
             else left = mid + 1;
+        }
+        return -1;
+    }
+
+
+    public static int binarySearchRecursive(int[] array, int left, int right, int target) {
+        if (right >= left && left <= array.length - 1) {
+            int mid = left + (right - left) / 2;
+            if (array[mid] == target)
+                return mid;
+            if (array[mid] > target)
+                return binarySearchRecursive(array, left, mid - 1, target);
+            return binarySearchRecursive(array, mid + 1, right, target);
         }
         return -1;
     }
--- src/test/java/by/trayan/algorithms/leetcode/chapter1/BinarySearchTest.java
+++ src/test/java/by/trayan/algorithms/leetcode/chapter1/BinarySearchTest.java
@@ -6,11 +6,20 @@ import org.springframework.util.Assert;
 class BinarySearchTest {
 
     @Test
-    void search() {
+    void searchBinary() {
         int[] toFind = {1, 4, 12, 21, 45, 123, 769};
 
         Assert.isTrue(BinarySearch.search(toFind, 4) == 1, "");
         Assert.isTrue(BinarySearch.search(toFind, 3) == -1, "");
         Assert.isTrue(BinarySearch.search(toFind, 45) == 4, "");
+    }
+
+    @Test
+    void searchBinaryRecursive() {
+        int[] toFind = {1, 4, 12, 21, 45, 123, 769};
+
+        Assert.isTrue(BinarySearch.binarySearchRecursive(toFind, 0, toFind.length - 1, 4) == 1, "");
+        Assert.isTrue(BinarySearch.binarySearchRecursive(toFind, 0, toFind.length - 1, 3) == -1, "");
+        Assert.isTrue(BinarySearch.binarySearchRecursive(toFind, 0, toFind.length - 1,45) == 4, "");
     }
 }

64a7ee60724402cb3cf6278ff7a219b0041900f5
polar <antronset@gmail.com>
2023-02-17 16:43:06 +0100

    Add MoveZeroes algo.

---
+++ src/main/java/by/trayan/algorithms/leetcode/chapter1/MoveZeroes.java
@@ -0,0 +1,34 @@
+package by.trayan.algorithms.leetcode.chapter1;
+
+/**
+ * Given an integer array nums, move all 0's to the end of it while maintaining the relative
+ * order of the non-zero elements.
+ * <p>
+ * Note that you must do this in-place without making a copy of the array.
+ * <p>
+ * Example 1:
+ * Input: nums = [0,1,0,3,12]
+ * Output: [1,3,12,0,0]
+ */
+
+public class MoveZeroes {
+
+    public static void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0) return;
+
+        int insertPos = 0;
+        for (int element: nums) {
+            if (element != 0) nums[insertPos++] = element;
+        }
+
+        while (insertPos < nums.length) {
+            nums[insertPos++] = 0;
+        }
+    }
+
+    private static void swap(int[] array, int changeFromIndex, int changeToIndex) {
+        int temp = array[changeFromIndex];
+        array[changeFromIndex] = array[changeToIndex];
+        array[changeToIndex] = temp;
+    }
+}
---
+++ src/test/java/by/trayan/algorithms/leetcode/chapter1/MoveZeroesTest.java
@@ -0,0 +1,20 @@
+package by.trayan.algorithms.leetcode.chapter1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MoveZeroesTest {
+
+    @Test
+    void moveZeroes() {
+        final int[] input = {0, 1, 0, 3, 12};
+        final int[] output = {1, 3, 12, 0, 0};
+
+        MoveZeroes.moveZeroes(input);
+        Assert.isTrue(Arrays.compare(input, output) == 0, "");
+    }
+}

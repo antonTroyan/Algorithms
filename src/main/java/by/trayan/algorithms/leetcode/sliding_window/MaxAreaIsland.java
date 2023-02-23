8ea39dadb0d0a164e60f93c3c8c9a5be94df80e9
polar <antronset@gmail.com>
2023-02-23 17:23:40 +0100

    Add MaxAreaIsland algo.

---
+++ src/main/java/by/trayan/algorithms/leetcode/sliding_window/MaxAreaIsland.java
@@ -0,0 +1,46 @@
+package by.trayan.algorithms.leetcode.sliding_window;
+
+/**
+ * You are given an m x n binary matrix grid. An island is a group of 1's (representing land)
+ * connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid
+ * are surrounded by water.
+ * <p>
+ * The area of an island is the number of cells with a value 1 in the island.
+ * <p>
+ * Return the maximum area of an island in grid. If there is no island, return 0.
+ * <p>
+ * Input: grid = [
+ * [0,0,1,0,0,0,0,1,0,0,0,0,0],
+ * [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ * [0,1,1,0,1,0,0,0,0,0,0,0,0],
+ * [0,1,0,0,1,1,0,0,1,0,1,0,0],
+ * [0,1,0,0,1,1,0,0,1,1,1,0,0],
+ * [0,0,0,0,0,0,0,0,0,0,1,0,0],
+ * [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ * [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+ * Output: 6
+ * Explanation: The answer is not 11, because the island must be connected 4-directionally.
+ */
+
+public class MaxAreaIsland {
+
+    public static int maxAreaOfIsland(int[][] grid) {
+        int max_area = 0;
+        for (int i = 0; i < grid.length; i++)
+            for (int j = 0; j < grid[0].length; j++)
+                if (grid[i][j] == 1)
+                    max_area = Math.max(max_area, AreaOfIsland(grid, i, j));
+        return max_area;
+    }
+
+    private static int AreaOfIsland(int[][] grid, int i, int j) {
+        if (i >= 0 && i < grid.length && j >= 0 && j < grid[0].length && grid[i][j] == 1) {
+            grid[i][j] = 0;
+            return 1 + AreaOfIsland(grid, i + 1, j)
+                    + AreaOfIsland(grid, i - 1, j)
+                    + AreaOfIsland(grid, i, j - 1)
+                    + AreaOfIsland(grid, i, j + 1);
+        }
+        return 0;
+    }
+}
---
+++ src/test/java/by/trayan/algorithms/leetcode/sliding_window/MaxAreaIslandTest.java
@@ -0,0 +1,26 @@
+package by.trayan.algorithms.leetcode.sliding_window;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaxAreaIslandTest {
+
+    @Test
+    void maxAreaOfIsland() {
+        final int[][] input = {
+                {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+                {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}
+        };
+        final int output = 6;
+
+        Assert.isTrue(MaxAreaIsland.maxAreaOfIsland(input) == 6,"");
+    }
+}
